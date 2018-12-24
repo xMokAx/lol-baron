124d15385ebdab21d2dea6558e60573691fc028a
xMokAx <ahmedmokhtar11j@gmail.com>
2018-12-24 18:10:17 +0200

    feature: adding pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: "lol-next",
+      script: "./server/server.js"
+    }
+  ],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "ec2-18-196-101-204.eu-central-1.compute.amazonaws.com",
+      key: "C:/Users/Moka/.ssh/lol-app.pem",
+      ref: "origin/master",
+      repo:
+        "https://git-codecommit.eu-central-1.amazonaws.com/v1/repos/lol-next",
+      path: "/home/ubuntu/lol-next",
+      "post-deploy":
+        "yarn install && yarn build && pm2 startOrRestart ecosystem.config.js"
+    }
+  }
+};
