08774720a964f0dfafa83e986dae24281290287b
xMokAx <ahmedmokhtar11j@gmail.com>
2019-01-01 18:18:56 +0200

    modify site manifest

--- pages/_document.js
+++ pages/_document.js
@@ -36,7 +36,7 @@ export default class MyDocument extends Document {
             sizes="16x16"
             href="/static/favicon/favicon-16x16.png"
           />
-          <link rel="manifest" href="/static/favicon/site.webmanifest" />
+          <link rel="manifest" href="/static/site.webmanifest" />
           <link
             rel="mask-icon"
             href="/static/favicon/safari-pinned-tab.svg"
