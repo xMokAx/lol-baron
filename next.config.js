f304786ae25dc6c68ce068273a54434bdbb3b1c7
xMokAx <ahmedmokhtar11j@gmail.com>
2019-01-09 19:42:30 +0200

    use bundle analyzer, remove images from precaching, use staleWhileRevalidate to cache gtm.js

--- next.config.js
+++ next.config.js
@@ -2,20 +2,38 @@ const withPlugins = require("next-compose-plugins");
 const withSass = require("@zeit/next-sass");
 const withCss = require("@zeit/next-css");
 const withOffline = require("next-offline");
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 
 module.exports = withPlugins([
+  [
+    withBundleAnalyzer,
+    {
+      analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: "static",
+          reportFilename: "../bundles/server.html"
+        },
+        browser: {
+          analyzerMode: "static",
+          reportFilename: "../bundles/client.html"
+        }
+      }
+    }
+  ],
   withCss,
   withSass,
   [
     withOffline,
     {
-      // to generate the service worker in development
+      // generate service worker in dev mode
       // generateInDevMode: true,
+      // Files matching against any of these patterns will be included in the precache manifest.
+      // globePatterns: "/static/**/*",
+      // The base directory you wish to match globPatterns against, relative to the current working directory.
+      // globeDirectory: "/.next",
       workboxOpts: {
-        // Files matching against any of these patterns will be included in the precache manifest.
-        globPatterns: ["static/**/*"],
-        // The base directory you wish to match globPatterns against, relative to the current working directory.
-        globDirectory: ".",
         // Whether or not the service worker should skip over the waiting lifecycle stage
         skipWaiting: true,
         // Whether or not the service worker should start controlling any existing clients as soon as it activates.
@@ -75,7 +93,8 @@ module.exports = withPlugins([
             }
           },
           {
-            urlPattern: /^((?!static).)*\.(?:png|jpg|jpeg|svg|gif)$/,
+            // urlPattern: /^((?!\/static).)*\.(?:png|jpg|jpeg|svg|gif)$/,
+            urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif|ico)/,
             handler: "cacheFirst",
             options: {
               cacheName: "image-cache",
@@ -105,6 +124,26 @@ module.exports = withPlugins([
               expiration: {
                 maxAgeSeconds: 60 * 60 * 24 * 365
               }
+            }
+          },
+          {
+            urlPattern: /\/(?:site.webmanifest|browserconfig.xml)$/,
+            handler: "cacheFirst",
+            options: {
+              cacheName: "pwa-cache",
+              cacheableResponse: {
+                statuses: [0, 200]
+              },
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/www\.googletagmanager\.com/,
+            handler: "staleWhileRevalidate",
+            options: {
+              cacheName: "gtm-cache"
             }
           }
         ]
